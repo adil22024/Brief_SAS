1c6093d57abe2b740686f3ae3e9c937a5430e695
adil22024 <alamiadil384@gmail.com>
2024-11-26 10:52:53 +0100

    ajout de la fonction ajouter et structure de tache

--- main.c
+++ main.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Tache{
+   
+   char titre[31];
+   char description[101];
+   char date[11];
+   char priorite[22];
+};
+
+int nombre_taches = 0;
+struct Tache taches[1000];
+
+void    ajouter_tache()
+{
+    struct Tache tache;
+
+    if (nombre_taches < 1000)
+    {
+        printf("Entrez le titre de la tache: ");
+        scanf(" %[^\n]", tache.titre);
+        printf("entrez la description de la tache: ");
+        scanf(" %[^\n]",tache.description);
+        printf("entrez la date de la tache: ");
+        scanf(" %[^\n]",tache.date);
+        printf("entrez la priorite de la tache: ");
+        scanf(" %[^\n]",tache.priorite);
+        taches[nombre_taches] = tache;
+        nombre_taches++;
+        printf("l'opération s'est terminée avec succes\n");
+    }
+
+}
+
 int main()
 {
     int choix;
@@ -14,5 +46,28 @@ int main()
         printf("4.supprimer une tache\n");
         printf("5.filtrer les taches\n");
         printf("6.quitter\n");
+         printf("\nentrer votre choix ");
+        scanf("%d", &choix);
+        switch (choix)
+        {
+            case 1:
+                ajouter_tache();
+                break ;
+            case 2:
+                break ;
+            case 3:
+                break ;
+            case 4:
+                break ;
+            case 5:
+                //
+                break ;
+            case 6:
+                exit(0);
+                break;
+            default:
+                printf("\ninvalide option\n\n");
+                break ;
+        }
     }
 }
