1c49e3014278dfb183a91de796d8ec4def29a3e3
adil22024 <alamiadil384@gmail.com>
2024-11-26 10:28:13 +0100

    Ajout de menu

--- main.c
+++ main.c
@@ -3,5 +3,16 @@
 
 int main()
 {
-    
+    int choix;
+
+    while (1)
+    {
+        printf("#.menu\n");
+        printf("1.ajouter une tache\n");
+        printf("2.afficher la liste des taches\n");
+        printf("3.modifier une tache\n");
+        printf("4.supprimer une tache\n");
+        printf("5.filtrer les taches\n");
+        printf("6.quitter\n");
+    }
 }
